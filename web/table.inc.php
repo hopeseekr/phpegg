2f73dc72fc13161289e898720ba5302ab05f306b
Un-Thesis <un-thesis@wakeup-people.com>
2001-04-15 16:42:35 +0000

    fixed html and style + fucking ^M's

--- web/table.inc.php
+++ web/table.inc.php
@@ -1,41 +1,29 @@
 <?php
 
 
-function create_table_top($width, $color, $table_name="<br>")
-	{
-	
-	$str="
+function create_table_top($width, $color, $table_name="<br>") {
+    $str="<center>\n".
+    "<table width=\"$width\" border=\"0\" cellspacing=\"0\" bgcolor=\"$color\">\n".
+    "<tr>\n".
+    "<td>\n".
+    "<font color=\"ffffff\">$table_name</font>\n".
+    "<table border=\"0\" cellspacing=\"0\" bgcolor=\"#ffffff\" width=\"100%\">\n".
+    "<tr>\n".
+    "<td width=\"30%\" valign=\"middle\">\n";
+    return $str;
+}
 
-<center>
-
-<table width=$width border=0 cellspacing=0 bgcolor=$color >  
- <tr>
-  <td>
-   <font color=ffffff>$table_name</font>
-
-     <table border=0 cellspacing=0 bgcolor=#ffffff width=100%>
-      <tr>
-       <td width=30% valign=middle>
-       ";
-       
-       return $str;
-       
-       }
-       
-function create_table_bottom()
-	{
-		
-		$str= "
-	       </td> 
-	      </tr>
-	     </table>
+function create_table_bottom() {		
+    $str= "</td>\n".
+    "</tr>\n".
+    "</table>\n".
 	   
-	    </td>
-	   </tr>
-	  </table>
-	  </center>
-	  ";
-	  return $str;
-  	}
+    "</td>\n".
+    "</tr>\n".
+    "</table>\n".
+    "</center>\n";
+    
+    return $str;
+}
   
- ?> 
+?>
