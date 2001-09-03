9e96ceb45bcc826cd9a638b6acac8d334bcc5a62
Un-Thesis <unthesis@wakeup-people.com>
2001-09-03 22:33:16 +0000

    Adds support for the db_ctrl interface

--- source/config.php
+++ source/config.php
@@ -7,6 +7,11 @@ $magic_word="HELLO!";
 $admin_email="user@localhost";
 
 // mysql database information 
+/* 
+  $database tells PHP-Egg what database you will be using it with.
+  The ONLY option right now: MySQL
+*/
+$database="MySQL";
 $database_host="localhost";
 $database_login="root";
 $database_pass="";
@@ -23,7 +28,7 @@ $irc_identd="phpa";
 $irc_real_name="phpegg beta 1";
 
 // quit message
-$quit_message="php_egg beta 1.2 by kill-9";
+$quit_message="php_egg beta 2 by kill-9 and Un-Thesis";
 
 // If you would like to run a server that is not part of the server groups now.
  //set $server_group="local"
@@ -34,6 +39,19 @@ $irc_server_ports[0]="6667";
 
 //$server_group="Random EU Undernet server";
 $server_group="local";
+
+/* This starts up the database 
+   WARNING: If your $database variable does not have something that matches the case statements 
+   below, your script WILL NOT work.
+*/
+include('db_ctrl.php');
+switch($database) {
+	case "MySQL":
+		$db_ctrl = new MySQLInterface;
+		$dbshort = "mysql";
+		break;
+}
+
 
 // If you are having any problems with getting php egg to connect to irc and dont know why 
 // you can uncomment the debug line below and see what it is that the bot is trying to do and mabie figure out why
